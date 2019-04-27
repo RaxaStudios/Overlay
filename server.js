5f5f3675a1b1ee5043aa90a8ede0a67794c6f8ed
Carl Camit <carlcamit0@gmail.com>
2019-04-27 10:24:37 +1000

    WIP

--- server.js
+++ server.js
@@ -1,62 +1,72 @@
-const express = require('express');
+const express = require("express");
 const mustacheExpress = require("mustache-express");
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const app = express();
-const request = require('request');
+const request = require("request");
 
-const EventEmitter = require('events');
-const WebSocket = require('ws');
+const EventEmitter = require("events");
+const WebSocket = require("ws");
 const server = new WebSocket.Server({ port: 8081 });
 
-const messageHandler = require('./messageHandler');
+const messageHandler = require("./messageHandler");
 const sub = new EventEmitter();
 
 exports.app = app;
 module.exports.sub = sub;
 
-server.on('connection', socket => {
-  socket.on('message', message => {
+server.on("connection", socket => {
+  socket.on("message", message => {
     var json = message;
     messageHandler.array.push(json);
     server.clients.forEach(client => {
       client.send(`message sent was:${message}`);
-    })
+    });
   });
-  socket.send('Connected to server at 8081');
+  socket.send("Connected to server at 8081");
 });
 
-app.engine('html', mustacheExpress());
+app.engine("html", mustacheExpress());
 app.use(express.static("public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
-app.set('views', './views');
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+function initialiseSSE(req, res) {
+  let messageId = 0;
 
-app.get('/api/events', function(req, res){
+  const intervalId = setInterval(() => {
+    res.write(`id: ${messageId}\n`);
+    res.write(`data: Test Message -- ${Date.now()}\n\n`);
+    messageId += 1;
+  }, 1000);
+
+  req.on("close", () => {
+    clearInterval(intervalId);
+  });
+}
+
+app.get("/event-stream", (req, res) => {
+  // SSE Setup
+  res.writeHead(200, {
+    "Content-Type": "text/event-stream",
+    "Cache-Control": "no-cache",
+    Connection: "keep-alive"
+  });
+  res.write("\n");
+
   initialiseSSE(req, res);
 });
 
-app.get('/', function (req, res) {
-  res.render('index');
+app.get("/", function(req, res) {
+  res.render("index");
 });
-
-function initialiseSSE(req, res) {
-  res.writeHead(200, {
-		"Content-Type": "text/event-stream",
-		"Cache-Control": "no-cache",
-		"Connection": "keep-alive"
-	});
-  sub.on("push", function(event, data){
-    res.write("data: " + data.msg + "\n\n");
-  });
-}
 
 /*test interval to send to browser
 setInterval(function(){
   sub.emit("push", "message", { msg: "message 1" });
 }, 5000);
 */
-app.listen(8080, function () {
-  console.log('sse app listening on port 8080!');
+app.listen(8080, function() {
+  console.log("sse app listening on port 8080!");
 });
